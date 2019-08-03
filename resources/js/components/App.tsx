3dd0bb23f36e796822c4c3476502605878ad24c1
Jakob Rolik <rolik.jakob@gmail.com>
2019-08-03 08:09:44 +0200

    Typescript setup

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button';
 
 
-class App extends Component {
+class App extends React.Component<{}, {
+  name: string
+  surname: string
+}> {
    state = {
-    name: "Jakob"
+    name: "Jakob",
+    surname: "Rolik"
   };
   render () {
     return (
       <div>
-        <h3>Welcome {this.state.name}</h3>
+        <h3>Welcome {this.state.surname} {this.state.name}</h3>
         <Button variant="contained" color="primary">Helo</Button>
       </div>
     )
